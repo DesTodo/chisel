89d69af895229bbdc8de5fc1087e185792ad70de
Desislava Todorova <dslavatodorova@gmail.com>
2017-05-31 14:42:52 -0600

    add Chisel class

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,13 +1,44 @@
-$ ruby ./lib/chisel.rb my_input.markdown my_output.html
+# $ ruby ./lib/chisel.rb my_input.markdown my_output.html
+# chisel.rb takes my_input_markdown and makes my_output_html
+# my_code = ./lib/chisel.rb
 
-chisel.rb takes my_input_markdown and makes my_output_html
-
-code takes markdown_input = File_path and parses to
-
-html_output = file_name 
-
-
+require './lib/chisel_files'
+#require './lib/my_input.markdown'
+require 'pry'
 
 
-#
-#Converted my_input.markdown (6 lines) to my_output.html (8 lines)
+class Chisel
+  #attr_reader :text
+
+  def initialize(markdown)
+    @text = FileRead.new(read)
+  end
+
+  def markdown_to_html
+    make_lines
+    make_chunks
+    chunk_to_html
+
+  end
+
+
+  def make_lines(markdown)
+    markdown_lines = text.split("\n")]
+  end
+
+  def make_chunks(markdown_lines)
+    markdown_chunks = text_lines.chomp
+  end
+
+  def chunk_to_html(markdown_chunks)
+    # remove empty lines
+    # translate  header"#" --> ""<h1>" ## == "<h2>" "###" == "<h3>"
+  end
+
+  # html_output = my_output.html
+chunk = Parser.new("./my_input.markdown")
+end
+
+binding.pry
+p chunks
+p chunk
